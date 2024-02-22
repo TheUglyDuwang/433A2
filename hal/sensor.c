27d3e4ca7afb4b15b852b7b930d535b83c8c85b2
Sunderinq <tegnoor.gill@hotmail.com>
2024-02-22 03:24:42 -0800

    added a simple getVoltage function

--- hal/sensor.c
+++ hal/sensor.c
@@ -1,1 +1,28 @@
 //implementation file for functions pertaining to the light sensor
+#include "sensor.h"
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#define A2D_FILE_VOLTAGE1 "/sys/bus/iio/devices/iio:device0/in_voltage1_raw"
+
+
+int getVoltage1Reading(){
+// Open file
+    FILE *f = fopen(A2D_FILE_VOLTAGE1, "r");
+    if (!f) {
+        printf("ERROR: Unable to open voltage input file. Cape loaded?\n");
+        printf(" Check /boot/uEnv.txt for correct options.\n");
+        exit(-1);
+    }
+    // Get reading
+    int a2dReading = 0;
+    int itemsRead = fscanf(f, "%d", &a2dReading);
+    if (itemsRead <= 0) {
+        printf("ERROR: Unable to read values from voltage input file.\n");
+        exit(-1);
+    }
+    // Close file
+    fclose(f);
+    return a2dReading;
+}
--- hal/sensor.h
+++ hal/sensor.h
@@ -1,1 +1,4 @@
 //header file containing the prototypes of functions used to read the light sensor values
+
+//get voltagereading
+int getVoltage1Reading();
