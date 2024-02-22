99c2eaea84b4398f96420df2053ac729fb5d36a9
TheUglyDuwang <mattfriesen2000@gmail.com>
2024-02-21 17:09:54 -0800

    Added functions to turn on and off LED, along with setting the duty cycle

--- hal/led.c
+++ hal/led.c
@@ -1,1 +1,33 @@
 //implementation of code used to turn the LED on and off
+
+#include "led.h"
+
+//enables the LED so it can turn on and off
+void turnOn(FILE* enable){//can probably turn the argument into a static in this file
+    int result = fprintf(enable, "1");
+    if(result <= 0){
+        printf("ERROR WRITING TO FILE\n");
+        exit(-1);
+    }
+    return;
+}
+
+//disables the LED to stop the blinking
+void turnOff(FILE* enable){
+    int result = fprintf(enable, "0");
+    if(result <= 0){
+        printf("ERROR WRITING TO FILE\n");
+        exit(-1);
+    }
+    return;
+}
+
+//sets the duty cycle of the LED
+void setDutyCycle(FILE* cycle, char* newCycle){
+        int result = fprintf(cycle, newCycle);
+    if(result <= 0){
+        printf("ERROR WRITING TO FILE\n");
+        exit(-1);
+    }
+    return;
+}
--- hal/led.h
+++ hal/led.h
@@ -1,1 +1,12 @@
 //file containing the function prototypes for led.c
+
+#include <cstdio>
+
+//enables the LED so it can turn on and off
+void turnOn(FILE* enable);
+
+//disables the LED to stop the blinking
+void turnOff(FILE* enable);
+
+//sets the duty cycle of the LED
+void setDutyCycle(FILE* cycle, int newCycle);
