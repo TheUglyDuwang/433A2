73cc93d656407d0a000876b3774e0f1f9ff0258f
Teg <tegnoor.gill@gmail.com>
2024-02-27 18:43:43 -0800

    cmake build

---
+++ build/CMakeFiles/as2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/as2.dir/app/blink.c.o"
+  "CMakeFiles/as2.dir/app/blink.c.o.d"
+  "CMakeFiles/as2.dir/app/lightReader.c.o"
+  "CMakeFiles/as2.dir/app/lightReader.c.o.d"
+  "CMakeFiles/as2.dir/app/main.c.o"
+  "CMakeFiles/as2.dir/app/main.c.o.d"
+  "CMakeFiles/as2.dir/hal/led.c.o"
+  "CMakeFiles/as2.dir/hal/led.c.o.d"
+  "CMakeFiles/as2.dir/hal/sensor.c.o"
+  "CMakeFiles/as2.dir/hal/sensor.c.o.d"
+  "as2"
+  "as2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/as2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/as2.dir/hal/sensor.c.o.d
@@ -0,0 +1,33 @@
+CMakeFiles/as2.dir/hal/sensor.c.o: \
+ /home/tegnoor/cmpt433/work/433A2/hal/sensor.c \
+ /usr/arm-linux-gnueabihf/include/stdc-predef.h \
+ /home/tegnoor/cmpt433/work/433A2/hal/sensor.h \
+ /usr/arm-linux-gnueabihf/include/stdlib.h \
+ /usr/arm-linux-gnueabihf/include/bits/libc-header-start.h \
+ /usr/arm-linux-gnueabihf/include/features.h \
+ /usr/arm-linux-gnueabihf/include/sys/cdefs.h \
+ /usr/arm-linux-gnueabihf/include/bits/wordsize.h \
+ /usr/arm-linux-gnueabihf/include/bits/long-double.h \
+ /usr/arm-linux-gnueabihf/include/gnu/stubs.h \
+ /usr/arm-linux-gnueabihf/include/gnu/stubs-hard.h \
+ /usr/lib/gcc-cross/arm-linux-gnueabihf/10/include/stddef.h \
+ /usr/arm-linux-gnueabihf/include/bits/waitflags.h \
+ /usr/arm-linux-gnueabihf/include/bits/waitstatus.h \
+ /usr/arm-linux-gnueabihf/include/bits/floatn.h \
+ /usr/arm-linux-gnueabihf/include/bits/floatn-common.h \
+ /usr/arm-linux-gnueabihf/include/bits/stdlib-float.h \
+ /usr/lib/gcc-cross/arm-linux-gnueabihf/10/include/stdbool.h \
+ /usr/arm-linux-gnueabihf/include/stdio.h \
+ /usr/lib/gcc-cross/arm-linux-gnueabihf/10/include/stdarg.h \
+ /usr/arm-linux-gnueabihf/include/bits/types.h \
+ /usr/arm-linux-gnueabihf/include/bits/timesize.h \
+ /usr/arm-linux-gnueabihf/include/bits/typesizes.h \
+ /usr/arm-linux-gnueabihf/include/bits/time64.h \
+ /usr/arm-linux-gnueabihf/include/bits/types/__fpos_t.h \
+ /usr/arm-linux-gnueabihf/include/bits/types/__mbstate_t.h \
+ /usr/arm-linux-gnueabihf/include/bits/types/__fpos64_t.h \
+ /usr/arm-linux-gnueabihf/include/bits/types/__FILE.h \
+ /usr/arm-linux-gnueabihf/include/bits/types/FILE.h \
+ /usr/arm-linux-gnueabihf/include/bits/types/struct_FILE.h \
+ /usr/arm-linux-gnueabihf/include/bits/stdio_lim.h \
+ /usr/arm-linux-gnueabihf/include/bits/sys_errlist.h
