e6db375644b591bd34fbe19de7d17bc2506f9eb5
TheUglyDuwang <mattfriesen2000@gmail.com>
2024-02-29 21:06:04 -0800

    Removed redundant function from blink, added function call for potentiometer. Changed isLooping from a parameter to an atomic variable to allow easier exiting

--- app/blink.c
+++ app/blink.c
@@ -5,20 +5,26 @@
 
 #include "blink.h"
 #include "led.h"
+#include "potentiometer.h"
+#include <stdatomic.h>
+
+
+static _Atomic bool isLooping;
 
 //main loop for blinking
-bool mainBlinkloop(bool isLooping){
+bool mainBlinkloop(void){
     enableLight();
-
+    isLooping = true;
     int newCycle = 0; //this will store the new cycle
-    FILE* cycle = fopen("/dev/bone/pwm/1/b/cycle", "w");
+    FILE* cycle; 
 
 
     while(isLooping){//isLooping can be changed via main thread when exiting
     
-    //get potentiometer here, get into newCycle
-
-        setCycle(newCycle, cycle);
+        newCycle = getVoltage0Reading();
+        cycle = fopen("/dev/bone/pwm/0/b/period", "w");
+        BLINK_C.setCycle(cycle, newCycle);
+        fclose(cycle);
 
     }
 
@@ -27,20 +33,9 @@ bool mainBlinkloop(bool isLooping){
     return true;
 }
 
-//uses the led file in the HAL folder to adjust the cycle
-void setCycle(int cycle, FILE* file){
-    char cycleChar[sizeof(cycle)*8+1];
-    itoa(cycle, cycleChar, DECIMAL);//converting in
-
-    int inputWritten = fprintf(file, cycleChar);//sets the cycle to the char-ized version of cycle
-    if(inputWritten <= 0){
-        printf("SOMETHING IS WRONG\n");
-    }
-}
-
 //turns on the light, allows it to blink
 bool enableLight(void){
-    FILE enableFile = fopen("/dev/bone/pwm/1/b/enable", "w");//file for light enabling
+    FILE enableFile = fopen("/dev/bone/pwm/0/b/enable", "w");//file for light enabling
     int inputWritten = fprintf(enableFile, "1");
     if(inputWritten <= 0){return false;}
     fclose(enableFile);
@@ -49,11 +44,15 @@ bool enableLight(void){
 
 //turns off light, stops it from blinking
 bool disableLight(void){
-    FILE enableFile = fopen("/dev/bone/pwm/1/b/enable", "w");
+    FILE enableFile = fopen("/dev/bone/pwm/0/b/enable", "w");
     int inputWritten = fprintf(enableFile, "0");
     if(inputWritten <= 0){return false;}
     fclose(enableFile);
     return true;
+}
+
+void stopLooping(void){
+    isLooping = false;
 }
 
 #endif
