80fd0c9497dc554ae5284b929186b0f74146c318
TheUglyDuwang <mattfriesen2000@gmail.com>
2024-02-29 17:38:22 -0800

    Added functions to read the potentiometer and get the voltage

---
+++ hal/potentiometer.h
@@ -0,0 +1,7 @@
+//header file for the potentiometer reading
+
+//gets the voltage reading on the potentiometer
+static int getVoltage0Reading(void);
+
+//gets the percent turned of the potentiometer
+double getPercent(void);
