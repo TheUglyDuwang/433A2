8f3359fae4b59d998f3572258a78446ea07b9a59
Teg <tegnoor.gill@gmail.com>
2024-02-29 19:54:44 -0800

    added comments

--- app/lightReader.c
+++ app/lightReader.c
@@ -2,8 +2,6 @@
 #include "sensor.h"
 #include "periodTimer.h"
 
-#include <pthread.h>
-#include <math.h> // for floor function
 
 #define VOLTAGE_MAX 4095
 #define REFERENCE_VOLTAGE 1.8
@@ -23,12 +21,14 @@ static pthread_t thread_id;
 static long long samplesTaken;
 static sampler reader; // declare reader as a global variable
 static sampler history;
-static history[1000];
-static long long totalVoltage;
 
+void copyArray(double source[], double destination[], int size) {
+    for (int i = 0; i < size; i++) {
+        destination[i] = source[i];
+    }
+}
 
-
-void Sampler_moveCurrentDataToHistory(void){
+void Sampler_moveCurrentDataToHistory(void) {
     history.size = reader.size;
     copyArray(reader.samples, history.samples, history.size);
     history.min = reader.min;
@@ -39,8 +39,7 @@ void Sampler_moveCurrentDataToHistory(void){
     return;
 }
 
-static long long getTimeInMs(void)
-{
+static long long getTimeInMs(void) {
     struct timespec spec;
     clock_gettime(CLOCK_REALTIME, &spec);
     long long seconds = spec.tv_sec;
@@ -49,44 +48,39 @@ static long long getTimeInMs(void)
     return milliSeconds;
 }
 
-long long Sampler_getNumSamplesTaken(void){
+long long Sampler_getNumSamplesTaken(void) {
     return samplesTaken;
 }
 
-double Sampler_getAverageReading(void){
+double Sampler_getAverageReading(void) {
     double avg = 0;
-    for(int i = 0; i < history.size; i++){
-        avg = avg + history.sample[i];
+    for (int i = 0; i < history.size; i++) {
+        avg = avg + history.samples[i];
     }
     avg = avg / history.size;
-    return floor( avg *1000)/ 1000;
+    return floor(avg * 1000) / 1000;
 }
 
-int Sampler_getHistorySize(void){
+int Sampler_getHistorySize(void) {
     return history.size;
 }
 
-double* Sampler_getHistory(int *size){
-    if(history.size = size){
-        return history.sample;
-    }
-    else{
-        perror("issue getting history");
-        return 0.00;
-    }
+double* Sampler_getHistory(int *size) {
+    *size = history.size;
+    return history.samples;
 }
 
-int Sampler_getDips( void ){
+int Sampler_getDips(void) {
     double avg = Sampler_getAverageReading();
     int dips = 0;
     int canCountDip = 1;
     int size = Sampler_getHistorySize();
 
-    for(int i = 0; i < size; i++){
-        if ( (avg - ar[i] ) < 0.1 && canCountDip){
+    for (int i = 0; i < size; i++) {
+        if ((avg - history.samples[i]) < 0.1 && canCountDip) {
             dips++;
             canCountDip = 0;
-        } else if ( (avg - ar[i] ) >= 0.07 && !canCountDip ) {
+        } else if ((avg - history.samples[i]) >= 0.07 && !canCountDip) {
             canCountDip = 1;
         }
     }
@@ -94,34 +88,35 @@ int Sampler_getDips( void ){
     return dips;
 }
 
-void terminal_OutputLine1( void ){
-    printf("Smpl ms[%d, %d] %f / %d",history.min, history.max, Sampler_getAverageReading(void), history.size );
+void terminal_OutputLine1(void) {
+    printf("Smpl ms[%f, %f] %f / %d\n", history.min, history.max, Sampler_getAverageReading(), history.size);
     return;
 }
-void terminal_OutputLine2( void ){
-    if(history.size < 20){
-        for (int i = 0; i < history.size; i++){
-            printf("%d : %f\n", i+1, history.sample[i]);
+
+void terminal_OutputLine2(void) {
+    if (history.size < 20) {
+        for (int i = 0; i < history.size; i++) {
+            printf("%d : %f\n", i + 1, history.samples[i]);
         }
-    } else{
-        int count = floor( sample.size);
-        for (int i = 0; i < history.size; i = i + count){
-            printf("%d : %f\n", i+1, history.sample[i]);
+    } else {
+        int count = floor(history.size);
+        for (int i = 0; i < history.size; i = i + count) {
+            printf("%d : %f\n", i + 1, history.samples[i]);
         }
     }
     return;
 }
 
-void *Sampler_thread(void* arg){
+void *Sampler_thread(void* arg) {
     int prevTime = getTimeInMs();
     int curTime = getTimeInMs();
     int prevTimePerSample;
     int curTimePerSample;
     int timeBetweenSamples;
     reader.min = 1000;
-    while(isInit){
+    while (isInit) {
         prevTimePerSample = getTimeInMs();
-        if( (curTime - prevTime) > 1000 ){
+        if ((curTime - prevTime) > 1000) {
             Sampler_moveCurrentDataToHistory();
             prevTime = getTimeInMs();
             terminal_OutputLine1();
@@ -134,10 +129,10 @@ void *Sampler_thread(void* arg){
         curTime = getTimeInMs();
         curTimePerSample = getTimeInMs();
         timeBetweenSamples = curTimePerSample - prevTimePerSample;
-        if(timeBetweenSamples < reader.min){
+        if (timeBetweenSamples < reader.min) {
             reader.min = timeBetweenSamples;
         }
-        if(timeBetweenSamples > reader.max){
+        if (timeBetweenSamples > reader.max) {
             reader.max = timeBetweenSamples;
         }
         usleep(1000); // sleep for 1 millisecond
@@ -145,22 +140,22 @@ void *Sampler_thread(void* arg){
     return NULL;
 }
 
-void Sampler_init(void){
+void Sampler_init(void) {
     isInit = 1;
     samplesTaken = 0;
     reader.size = 0;
     history.size = 0;
-    totalVoltage = 0;
     if (pthread_create(&thread_id, NULL, Sampler_thread, NULL) != 0) {
         perror("pthread_create");
         return;
     }
 }
 
-void Sampler_cleanup(void){
+void Sampler_cleanup(void) {
     isInit = 0;
     if (pthread_join(thread_id, NULL) != 0) {
         perror("pthread_join");
         return;
     }
+    // Add any necessary cleanup here, such as freeing dynamically allocated memory
 }
--- app/lightReader.h
+++ app/lightReader.h
@@ -1,29 +1,41 @@
-//header file for lightReader.c
+#ifndef LIGHT_READER_H
+#define LIGHT_READER_H
 
-#include <stdio.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <math.h>
-#include <stdlib.h> 
+#include <math.h> // for floor function
+#include <stdio.h> // for printf
+#include <stdlib.h> // for NULL
+#include <time.h> // for clock_gettime
+#include <unistd.h> // for usleep
 
+// Initializes the sampler
+void Sampler_init(void);
 
-void Sampler_init(void);//
-void Sampler_cleanup(void);//
+// Cleans up the sampler
+void Sampler_cleanup(void);
 
-void Sampler_moveCurrentDataToHistory(void);//
+// Moves current data to history
+void Sampler_moveCurrentDataToHistory(void);
 
-int Sampler_getHistorySize(void);//
+// Gets the size of the history
+int Sampler_getHistorySize(void);
 
-double* Sampler_getHistory(int *size);//
+// Gets the history of samples and returns the size
+double* Sampler_getHistory(int *size);
 
-double Sampler_getAverageReading(void);//
+// Calculates and returns the average reading from the history
+double Sampler_getAverageReading(void);
 
-long long Sampler_getNumSamplesTaken(void);//
+// Returns the total number of samples taken
+long long Sampler_getNumSamplesTaken(void);
 
-int Sampler_getDips( void );//
+// Returns the number of dips detected in the history
+int Sampler_getDips(void);
 
+// Outputs the first line of data to the terminal
 void terminal_OutputLine1(void);
 
+// Outputs the second line of data to the terminal
 void terminal_OutputLine2(void);
 
-
+#endif /* LIGHT_READER_H */
